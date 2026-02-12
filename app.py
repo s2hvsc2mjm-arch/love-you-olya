a1f022e6cc129f31f32196db56b329e8b3237288
s2hvsc2mjm-arch <s2hvsc2mjm@privaterelay.appleid.com>
2026-02-12 12:40:45 +0300

    Simplify index route by removing date calculations
    Removed date calculation logic and simplified index route.

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Дата начала
-    start_date = datetime(2024, 4, 4, 22, 10)
-    now = datetime.now()
-    diff = now - start_date
-    
-    # Передаем данные
-    data = {
-        "days": diff.days,
-        "hours": int(diff.total_seconds() // 3600),
-        "minutes": int(diff.total_seconds() // 60),
-        "seconds": int(diff.total_seconds())
-    }
-    return render_template('index.html', data=data)
+    # Теперь мы просто отдаем страницу, а JS сам всё посчитает
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
